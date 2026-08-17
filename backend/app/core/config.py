ef0be2c606e087f60e3847a5a80b824842b981e7
susesh <susesh1006@gmail.com>
2026-08-17 14:37:21 +0530

    changed the model to new model

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,6 +6,6 @@ load_dotenv()
 class Settings:
     GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
     
-    MODEL_NAME: str = "llama-3.1-8b-instant"
+    MODEL_NAME: str = "openai/gpt-oss-20b"
 
 settings = Settings()
